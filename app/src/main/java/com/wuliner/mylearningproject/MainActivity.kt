67064315cabb2787a3fa6a5e997a500b4a9859e3
wuliner <3302089539@qq.com>
2023-07-30 15:24:44 +0800

    dev_DerawerLayout: 学习使用toolbar, 添加menu到toolbar

--- app/src/main/java/com/wuliner/mylearningproject/MainActivity.kt
+++ app/src/main/java/com/wuliner/mylearningproject/MainActivity.kt
@@ -2,6 +2,9 @@ package com.wuliner.mylearningproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.wuliner.mylearningproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,4 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.backup -> Toast.makeText(this, "You clicked Backup",
+                Toast.LENGTH_SHORT).show()
+                    R.id.delete -> Toast.makeText(this, "You clicked Delete",
+                Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "You clicked Settings",
+                Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
 }
