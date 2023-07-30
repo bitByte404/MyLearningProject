7a3c132e9196650bbd49ba993da15995ef958b07
wuliner <3302089539@qq.com>
2023-07-30 09:40:52 +0800

    dev_listView: 简单的使用listView

--- app/src/main/java/com/wuliner/mylearningproject/MainActivity.kt
+++ app/src/main/java/com/wuliner/mylearningproject/MainActivity.kt
@@ -2,10 +2,21 @@ package com.wuliner.mylearningproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.wuliner.mylearningproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val data = listOf("Appple", "Banana", "Orange", "Watermelon", "Pear",
+        "Grape", "PineAple", "Strawberry", "Cherry", "Mango", "Appple", "Banana",
+        "Orange", "Watermelon", "Pear", "Grape", "PineAple", "Strawberry", "Cherry", "Mango")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        binding.listView.adapter = adapter
+
+        setContentView(binding.root)
     }
 }
