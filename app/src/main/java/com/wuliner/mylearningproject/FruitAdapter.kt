b425ee35ae59b9e2d0318e4231ae0d58fc45ab9b
wuliner <3302089539@qq.com>
2023-07-30 10:09:46 +0800

    dev_listView: 在getView中使用convertView复用加载好的布局，提高性能

--- app/src/main/java/com/wuliner/mylearningproject/FruitAdapter.kt
+++ app/src/main/java/com/wuliner/mylearningproject/FruitAdapter.kt
@@ -2,6 +2,7 @@ package com.wuliner.mylearningproject
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,9 +13,28 @@ import android.widget.TextView
 class FruitAdapter(activity: Activity, val resourceId: Int, data: List<Fruit>):
     ArrayAdapter<Fruit>(activity, resourceId, data) {
 
+    /*
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+        val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
+        val fruitName: TextView = view.findViewById(R.id.fruitName)
+        val fruit = getItem(position)//获取当前项的Fruit实例
+        if (fruit != null) {
+            fruitImage.setImageResource(fruit.imageId)
+            fruitName.text = fruit.name
+        }
+        return view
+    }
+     */
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View { //convertView用于将之前加载好的布局花村
+        val view: View
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+        } else {
+            view = convertView
+        }
         val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
         val fruitName: TextView = view.findViewById(R.id.fruitName)
         val fruit = getItem(position)//获取当前项的Fruit实例
