2eaf9536128b3cad4528112f3216964a1a281c96
wuliner <3302089539@qq.com>
2023-07-30 10:01:26 +0800

    dev_listView: 使用自定义adapter，自定义listView的样式

---
+++ app/src/main/java/com/wuliner/mylearningproject/Fruit.kt
@@ -0,0 +1,5 @@
+package com.wuliner.mylearningproject
+
+class Fruit(val name: String, val imageId: Int) {
+
+}
--- app/src/main/java/com/wuliner/mylearningproject/MainActivity.kt
+++ app/src/main/java/com/wuliner/mylearningproject/MainActivity.kt
@@ -10,13 +10,30 @@ class MainActivity : AppCompatActivity() {
     private val data = listOf("Appple", "Banana", "Orange", "Watermelon", "Pear",
         "Grape", "PineAple", "Strawberry", "Cherry", "Mango", "Appple", "Banana",
         "Orange", "Watermelon", "Pear", "Grape", "PineAple", "Strawberry", "Cherry", "Mango")
+
+    private val fruitList = ArrayList<Fruit>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        initFruits()//初始化水果数据
+        val adapter = FruitAdapter(this, R.layout.fruit_item, fruitList)
         binding.listView.adapter = adapter
 
         setContentView(binding.root)
+    }
+
+    private fun initFruits() {
+        repeat(2) {
+            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
+            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
+            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
+            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
+            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
+            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
+            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
+            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
+            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+        }
     }
 }
