a534ecd7688aacf4faa5b790289f6e07af30e889
wuliner <3302089539@qq.com>
2023-07-30 10:21:16 +0800

    dev_listView: 通过ViewHolder将所有的控件实例缓存，提高运行效率

--- app/src/main/java/com/wuliner/mylearningproject/FruitAdapter.kt
+++ app/src/main/java/com/wuliner/mylearningproject/FruitAdapter.kt
@@ -9,11 +9,16 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 class FruitAdapter(activity: Activity, val resourceId: Int, data: List<Fruit>):
     ArrayAdapter<Fruit>(activity, resourceId, data) {
 
+    inner  class ViewHolder(val fruitImage: ImageView, val fruitName: TextView)
+
+
     /*
+    版本一
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
@@ -28,6 +33,8 @@ class FruitAdapter(activity: Activity, val resourceId: Int, data: List<Fruit>):
     }
      */
 
+    /*
+    版本二 使用convertView节约性能
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View { //convertView用于将之前加载好的布局花村
         val view: View
         if (convertView == null) {
@@ -41,6 +48,31 @@ class FruitAdapter(activity: Activity, val resourceId: Int, data: List<Fruit>):
         if (fruit != null) {
             fruitImage.setImageResource(fruit.imageId)
             fruitName.text = fruit.name
+        }
+        return view
+    }
+     */
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View { //convertView用于将之前加载好的布局缓存
+
+
+        val view: View
+        val viewHolder: ViewHolder
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+            val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
+            val fruitName: TextView = view.findViewById(R.id.fruitName)
+            viewHolder = ViewHolder(fruitImage, fruitName)
+            view.tag = viewHolder
+
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+        val fruit = getItem(position)//获取当前项的Fruit实例
+        if (fruit != null) {
+            viewHolder.fruitImage.setImageResource(fruit.imageId)
+            viewHolder.fruitName.text = fruit.name
         }
         return view
     }
